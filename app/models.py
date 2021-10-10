c0b68dc00847b1d2caf67d58315b279efa91c780
Anaya Pandit <anaya.pandit7924@gmail.com>
2021-10-09 18:40:05 -0700

    Added FLASK_APP under .flaskenv

--- app/models.py
+++ app/models.py
@@ -19,7 +19,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     role = db.Column(db.Integer, index=False, unique=False) # 0 is Teacher, 1 is Student
-    about_me = db.Column(db.String(140))
+    about_me = db.Column(db.String(140), nullable=True)
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author', lazy='dynamic') # First part is calling it from the Post model, secret field is the backref (author)
     reactions = db.relationship('Reactions', backref='reactor', lazy='dynamic') # This will show all the reactions that this user has made
